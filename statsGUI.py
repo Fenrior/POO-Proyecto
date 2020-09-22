ed886a1660f12be2860856b0a3f6d949b490b19b
Andrés Ignacio Cortez Ayerdi <60154577+Fenrior@users.noreply.github.com>
2020-09-21 18:19:15 -0600

    Update statsGUI.py

--- statsGUI.py
+++ statsGUI.py
@@ -25,3 +25,37 @@ class VerInfo(tk.Tk):
         ingresar_archivo = tk.Entry(self, textvariable=self.archivo)
         self.lb = tk.Label(self, bg="#74bce3", foreground="#f7b80a", text="")
 
+        self.titulo.grid(row=0, column=0, columnspan=4)
+        descarga.grid(row=1, column=0, sticky=tk.W+tk.E, padx=5)
+        ingresar_archivo.grid(row=2, column=1, columnspan=3, sticky=tk.W+tk.E, padx=10)
+        self.lb.grid(row=1, column=1, columnspan=3,  sticky=tk.W+tk.E)
+        self.excel.grid(row=2, column=0, sticky=tk.W+tk.E, padx=5)
+        self.graphs.grid(row=3, column=0, sticky=tk.W+tk.E, padx=5)
+
+    
+    def download(self):
+        self.manejo.descargar_info()
+        if self.manejo.downloaded:
+            self.lb.config(text="Se ha descargado la información")
+            self.excel.config(state=tk.ACTIVE)
+            self.graphs.config(state=tk.ACTIVE)
+        else:
+            self.lb.config(text="No se ha podido descargar la información")
+
+    def create(self):
+        direc = fd.askdirectory(title="Seleccionar Carpeta")
+        if direc:
+            if self.archivo.get() != "":
+                self.manejo.crear_excel(direc+"/", self.archivo.get())
+            else:
+                self.manejo.crear_excel(direc, "default_name")
+            self.lb.config(text="Se ha creado el archivo")
+
+    def graphicate(self):
+        self.manejo.bar_graph()
+
+
+
+if __name__ == "__main__":
+    vv = VerInfo("")
+    vv.mainloop()
