8074691661218efd4c19a58b54089735263562ce
Andrés Ignacio Cortez Ayerdi <60154577+Fenrior@users.noreply.github.com>
2020-09-21 18:11:20 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Andrés Cortez, Fernando Lavarreda, Valeria Paiz, Alejandro Ortega
+# Universidad del Valle, Programacion Orientada a Objetos
+# Punto de partida, programq que representa una aplicación para determinar si un usuario padece de alguna enfermedad
+# mental.
+
+import inicio as st
+import analytics as ant
